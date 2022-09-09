ca34b0544abfff6f26b1c907cb92bb6440a00d0a
Tommy Kang <tommykang77@naver.com>
2022-09-09 10:51:28 +0900

    Create function to play single round
    Create a function playSingleRound to play a single round
    of rock paper scissors and return an appropriate string
    depending on the outcome of the game.

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
+let wins = {
+    "Rock": "Paper",
+    "Paper": "Scissors",
+    "Scissors": "Rock"
+}
+
+// randomly choose a choice for computer
 function getComputerChoice() {
     let choices = ["Rock", "Paper", "Scissors"];
     let randomIndex = Math.floor(Math.random() * 3);
     return choices[randomIndex];
+}
+
+// a single round of rock paper scissors
+function playSingleRound(playerSelection, computerSelection) {
+    // if tied
+    if (playerSelection === computerSelection)
+        return "Tie!";
+    
+    // if player wins
+    if (playerSelection === wins[computerSelection])
+        return "You Win! " + playerSelection + " beats " + computerSelection;
+    
+    // if player loses
+    return "You Lose! " + computerSelection + " beats " + playerSelection;
 }
